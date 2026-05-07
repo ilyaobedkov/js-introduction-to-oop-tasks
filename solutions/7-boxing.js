252383dd622b304686ac962707f40754aec41b0d
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:46:19 +0300

    7-boxing.js

--- solutions/7-boxing.js
+++ solutions/7-boxing.js
@@ -1,3 +1,13 @@
-// BEGIN
-
-// END
+export default function magic(...args) {
+  // Суммируем все переданные аргументы
+  let sum = args.reduce((acc, num) => acc + num, 0);
+  // Создаем функцию, которая будет принимать следующие аргументы
+  const func = (...nextArgs) => {
+    // Добавляем новые аргументы к общей сумме
+    const nextSum = nextArgs.reduce((acc, num) => acc + num, 0);
+    // Возвращаем новую функцию с обновленной суммой
+    return magic(sum + nextSum);
+  };
+  func.valueOf = () => sum;
+  return func;
+}
