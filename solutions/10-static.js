5c271d1511dad3fed1f59fc4d43d010c2dd82078
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:48:12 +0300

    10-static.js

--- solutions/10-static.js
+++ solutions/10-static.js
@@ -1,13 +1,12 @@
 export default class Time {
-  // BEGIN
-
-  // END
-
+  static fromString(str) {
+    const [hours, minutes] = str.split(':'); // Разделяем строку по двоеточию
+    return new Time(parseInt(hours, 10), parseInt(minutes, 10)); // Создаем новый объект Time
+  }
   constructor(hours, minutes) {
     this.minutes = minutes;
     this.hours = hours;
   }
-
   toString() {
     return `${this.hours}:${this.minutes}`;
   }
