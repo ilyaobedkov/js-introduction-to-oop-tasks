bddeebd1cedc02c6e91535d8c562f6123a532e80
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:44:12 +0300

    3-bind.js

--- solutions/3-bind.js
+++ solutions/3-bind.js
@@ -1,3 +1,8 @@
-// BEGIN
-
-// END
+const bind = (obj, fn) => {
+  // возвращаем новую функцию, которая при вызове будет привязана к obj
+  return (...args) => {
+    // используем apply, чтобы вызвать fn с контекстом obj и переданными аргументами
+    return fn.apply(obj, args);
+  };
+};
+export default bind;
