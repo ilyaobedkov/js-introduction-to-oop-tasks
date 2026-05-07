43719486b25afc72718e00ae0a643e45008a8de9
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:43:43 +0300

    2-this.js

--- solutions/2-this.js
+++ solutions/2-this.js
@@ -1,3 +1,18 @@
-// BEGIN
-
-// END
+const make = (numer = null, denom = null) => ({
+  numer,
+  denom,
+  setNumer(value) { this.numer = value; },
+  setDenom(value) { this.denom = value; },
+  getNumer() { return this.numer; },
+  getDenom() { return this.denom; },
+  toString() { return `${this.numer}/${this.denom}`; },
+  add(other) {
+    // a/b + c/d = (a*d + b*c) / (b*d)
+    const a = this.numer;
+    const b = this.denom;
+    const c = other.getNumer();
+    const d = other.getDenom();
+    return make(a * d + b * c, b * d);  // вызываем make внутри себя
+  }
+});
+export default make;
