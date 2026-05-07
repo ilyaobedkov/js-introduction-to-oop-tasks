457ba1e0252c6fc3e11aa5da9c570ac34e401b81
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:47:52 +0300

    9-class.js

--- solutions/9-class.js
+++ solutions/9-class.js
@@ -1,5 +1,25 @@
-import _ from 'lodash';
-
-// BEGIN
-
-// END
+export default class Cart {
+  constructor() {
+    this.items = []; // Хранилище товаров
+  }
+  addItem(item, count) {
+    this.items.push({ item, count }); // Добавляем товар и количество
+  }
+  getItems() {
+    return this.items; // Возвращаем массив товаров
+  }
+  getCost() {
+    let total = 0;
+    for (const { item, count } of this.items) {
+      total += item.price * count; // Суммируем цену * количество
+    }
+    return total;
+  }
+  getCount() {
+    let total = 0;
+    for (const { count } of this.items) {
+      total += count; // Суммируем количество
+    }
+    return total;
+  }
+}
