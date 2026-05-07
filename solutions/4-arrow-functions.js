060448a6fe6381c539ceebb5796ca13aabd717a1
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-05-07 20:44:40 +0300

    4-arrow-functions.js

--- solutions/4-arrow-functions.js
+++ solutions/4-arrow-functions.js
@@ -1,3 +1,7 @@
-// BEGIN
-
-// END
+const each = (collection, callback) => {
+  for (let i = 0; i < collection.length; i++) {
+    // вызываем callback с контекстом, равным текущему объекту коллекции
+    callback.call(collection[i]);
+  }
+};
+export default each;
